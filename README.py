25996c84e626d0355eab97ccf4c1abeb381beec4
Matveev Maxim <dbhsmq@gmail.com>
2025-12-30 20:21:48 +0300

    убрал все лишнее

--- README.py
+++ README.py
@@ -1,6 +1,3 @@
-# test_rep
-print("hello")
-print ("1111111")
-# ddddd rr rr
-#dsfsdfdsfds 1 1 новое изменение  еще одно 23142134234234324234234234
-#bpvtytybz CON-1
+# test_rep 
+# Подчистил новый код
+print("Новая версия")
